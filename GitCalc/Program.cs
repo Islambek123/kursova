2b1e80df4afb0d22ed9b73557540187c3a73df70
Vitalik <topkokos1337@gmail.com>
2017-10-25 17:14:56 +0300

    Update Name Form

--- GitCalc/Program.cs
+++ GitCalc/Program.cs
@@ -16,7 +16,7 @@ namespace GitCalc
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
